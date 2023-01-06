4bdf78d909df968183f4d0a89c58246b117908c6
SantanuDas2345 <santanudasricxy123@gmail.com>
2023-01-06 19:46:35 +0530

    upadting index.jsx file

--- src/component/Display/index.jsx
+++ src/component/Display/index.jsx
@@ -3,7 +3,7 @@ import "./display-style.css";
 export default function Display() {
   return (
     <div id="display">
-      <iframe id="result" className="hei-wid-100P"></iframe>
+      <iframe id="result" className="hei-wid-100P" title={'result'}></iframe>
     </div>
   );
 }
